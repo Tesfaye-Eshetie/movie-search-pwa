9a8f9c2e22039ce35d8affba2d7bcae15459c45b
tesfaye eshetie <tesfayeeshetie19@gmail.com>
2022-09-09 21:39:28 -0400

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/vite-app-sample/',
+  base: '/movie-search-pwa/',
   plugins: [react()],
 });
