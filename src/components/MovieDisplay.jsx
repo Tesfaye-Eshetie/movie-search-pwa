00fc1ccb16c2200c3d23d4cf6a8e044485a38f58
tesfaye eshetie <tesfayeeshetie19@gmail.com>
2022-09-13 11:44:47 -0400

    updated search page

--- src/components/MovieDisplay.jsx
+++ src/components/MovieDisplay.jsx
@@ -6,7 +6,7 @@ import './MovieDisplay.css'
 import AddFavortite from './AddFavortite.jsx';
 
 export default function MovieDisplay() {
-  const [movie, setMovie] = useState([]);
+  const [movie, setMovie] = useState();
   
   const getSearchMovie = async () => {
     (await database).get('searchMovie', 'search')
@@ -23,16 +23,14 @@ export default function MovieDisplay() {
   
   return (
     <section className="display-container" id="display" >
-      {
+      { movie && (
         movie.Response === 'False' && <NoResult key={movie.Response}/> ||
         movie.Response === 'True' && 
           <div className="display-div">
             <MovieCard movie={movie} store='searchMovie' id='search' />
             <AddFavortite movie={movie}/>
           </div>
-        //       <button className='fav-button green-button' onClick={addFavorite}>add to fav</button>
-        //       <button className='fav-button red-button display-none' onClick={() => removeFavorite(movie) }>Remove from fav</button>
-      
+      )
       } 
     </section>
   )
